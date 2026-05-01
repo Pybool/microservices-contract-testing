3c8d882b3049ddf55d21d1c7f13bd9eef09367a4
Pybool <ekoemmanueljavl@gmail.com>
2026-05-01 14:16:57 +0100

    Feat: Fix github actions secrets

--- consumer-node/src/pactbrokerChecker.ts
+++ consumer-node/src/pactbrokerChecker.ts
@@ -3,7 +3,7 @@ import { config } from "./config";
 const BROKER_URL = config?.PACT_BROKER_URL || "http://16.171.160.56:9292";
 
 export async function checkBroker() {
-  console.log("BROKER_URL ==> ", BROKER_URL)
+  console.log(`BROKER_URL ==>  ${BROKER_URL}`)
   try {
     const res = await fetch(BROKER_URL);
     if (!res.ok) {
