aa0fe83b74879e96ab7210ba3b75b3b69572028b
Pybool <ekoemmanueljavl@gmail.com>
2026-05-01 13:52:50 +0100

    Feat: Fix github actions secrets

--- orders-service/src/config.ts
+++ orders-service/src/config.ts
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const config = {
-  BROKER_URL: "http://localhost:9292",
+  BROKER_URL: process.env.PACT_BROKER_URL || "http://localhost:9292",
 };
